7019e821929304459b65302d94a96fe5d89ca169
Charlie Gray <charliechasegray@gmail.com>
2023-12-30 22:33:28 -0700

    finish lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, anagrams):
+        return [anagram for anagram in anagrams if sorted(anagram.lower()) == self.sorted_word and anagram.lower() != self.word]
